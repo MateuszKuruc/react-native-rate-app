406f75a875867d169d815f43911f0086166e0d29
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-08 19:03:42 +0200

    add basic structure for display

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,10 +1,30 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, Button } from "react-native";
+import Constants from "expo-constants";
 
 const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+  },
   avatar: {
     width: 50,
     height: 50,
   },
+  upperWrapper: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  languageContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1877F2",
+    padding: 5,
+  },
+  languageInfo: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  detailsContainer: {},
 });
 
 const RepositoryItem = ({
@@ -18,20 +38,27 @@ const RepositoryItem = ({
   ownerAvatarUrl,
 }) => {
   return (
-    <View>
-      <Text>Full name: {fullName}</Text>
-      <Text>Description: {description}</Text>
-      <Text>Language: {language}</Text>
+    <View style={styles.container}>
+      <View style={styles.upperWrapper}>
+        <Image
+          source={{
+            uri: ownerAvatarUrl,
+          }}
+          style={styles.avatar}
+        ></Image>
+        <View style={styles.detailsContainer}>
+          <Text>{fullName}</Text>
+          <Text>{description}</Text>
+          <View style={styles.languageContainer}>
+            <Text style={styles.languageInfo}>{language}</Text>
+          </View>
+        </View>
+      </View>
+
       <Text>Stars: {stargazersCount}</Text>
       <Text>Forks: {forksCount}</Text>
       <Text>Reviews: {reviewCount}</Text>
       <Text>Rating: {ratingAverage}</Text>
-      <Image
-        source={{
-          uri: ownerAvatarUrl,
-        }}
-        style={styles.avatar}
-      ></Image>
     </View>
   );
 };
