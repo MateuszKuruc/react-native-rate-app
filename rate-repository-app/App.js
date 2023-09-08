a938289cb4a13b8b507a6b8c956b007e93626cf1
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-08 13:32:45 +0200

    ex 10.3 done, add components for displaying repository list and individual items

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Main from "./src/components/Main";
+import RepositoryList from "./src/components/RepositoryList";
+import { View } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Test rate-repository-app</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Main />
+      <RepositoryList />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
