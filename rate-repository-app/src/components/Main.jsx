51b8aa503913554be3aa8664ad358b97bf96adea
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-09 11:06:47 +0200

    add singin component and add appropriate route path for it

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate } from "react-router-native";
 
 import AppBar from "./AppBar";
 import RepositoryList from "./RepositoryList";
+import SignIn from "./SignIn";
 import theme from "../theme";
 
 const styles = StyleSheet.create({
@@ -20,6 +21,7 @@ const Main = () => {
       <Routes>
         <Route path="/" element={<RepositoryList />} exact />
         <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/signin" element={<SignIn />} exact />
       </Routes>
     </View>
   );
