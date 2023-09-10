32ef9582a0e7f3f931af9b7154dcee1be5a5a1cc
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-10 13:53:47 +0200

    ex 10.9, add validation for login form using yup, add error messages

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -4,6 +4,8 @@ import { View, StyleSheet } from "react-native";
 import { Formik } from "formik";
 import Button from "./Button";
 
+import * as yup from "yup";
+
 const styles = StyleSheet.create({
   container: {
     padding: 10,
@@ -14,6 +16,19 @@ const initialValues = {
   username: "",
   password: "",
 };
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(4, "Username needs to be at least 4 characters long!")
+    .max(20, "Username cannot be longer than 20 characters!")
+    .required("Username is required!"),
+  password: yup
+    .string()
+    .min(6, "Password needs to be at least 6 characters long!")
+    .max(20, "Password cannot be longer than 20 characters!")
+    .required("Password is required!"),
+});
 
 const LoginForm = ({ onSubmit }) => {
   return (
@@ -31,7 +46,11 @@ const SignIn = () => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
       {({ handleSubmit }) => <LoginForm onSubmit={handleSubmit} />}
     </Formik>
   );
