6dcdd223cf0a73a0f145946116b20a44495638b4
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-10 12:33:06 +0200

    ex 10.8, add loginform with formik, add basic styling of form

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,36 @@
 import Text from "./Text";
+import FormikTextInput from "./FormikTextInput";
+
+import { Pressable, View, StyleSheet } from "react-native";
+import { Formik } from "formik";
+
+const initialValues = {
+  username: "",
+  password: "",
+};
+
+const LoginForm = ({ onSubmit }) => {
+  return (
+    <View>
+      <FormikTextInput name="username" placeholder="username" />
+      <FormikTextInput name="password" placeholder="password" />
+      <Pressable onPress={onSubmit}>
+        <Text>Sign in</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <LoginForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
--- rate-repository-app/src/components/TextInput.jsx
+++ rate-repository-app/src/components/TextInput.jsx
@@ -1,9 +1,17 @@
 import { TextInput as NativeTextInput, StyleSheet } from "react-native";
+import theme from "../theme";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  field: {
+    borderColor: theme.colors.textSecondary,
+    borderWidth: 1,
+    fontFamily: theme.fonts.main,
+    padding: 10,
+  },
+});
 
 const TextInput = ({ style, error, ...props }) => {
-  const textInputStyle = [style];
+  const textInputStyle = [styles.field, style];
 
   return <NativeTextInput style={textInputStyle} {...props} />;
 };
